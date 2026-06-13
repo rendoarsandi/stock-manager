481f97a54948294da502a66783dd5fcbfee1ad35
Rendo Arsandi <rendoarsandi@gmail.com>
2026-06-13 13:07:59 +0700

    Fix Clerk SSR integration

--- app/ssr.jsx
+++ app/ssr.jsx
@@ -1,8 +1,11 @@
 import { createStartHandler, defaultStreamHandler } from '@tanstack/react-start/server';
 import { getRouterManifest } from '@tanstack/react-start/router-manifest';
 import { createRouter } from './router';
+import { createClerkHandler } from '@clerk/tanstack-react-start/server';
 
-export default createStartHandler({
+const handler = createStartHandler({
   createRouter,
   getRouterManifest,
 })(defaultStreamHandler);
+
+export default createClerkHandler(handler);
